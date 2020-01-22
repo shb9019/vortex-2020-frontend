d86419529e390b92b4ca82fa758e34df6e672c91
AnjaneyaTripathi <anjaneya532000@gmail.com>
2020-01-22 23:07:18 +0530

    forgot password email assistance

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -1,60 +1,96 @@
-import * as React from 'react';
+import * as React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import '../styles/ForgotPassword.css';
-import {Col, Row} from "react-bootstrap";
+import "../styles/ForgotPassword.css";
+import { Col, Row } from "react-bootstrap";
 
 export default function ForgotPassword() {
-    const [email, changeEmail] = React.useState("");
+  const [email, changeEmail] = React.useState("");
+  const [successMessage, changeSuccess] = React.useState("");
+  const [errorMessage, changeError] = React.useState("");
 
-    const sendResetLink = () => {
-        fetch("http://localhost:8000/api/user/forgotPassword", {
-            method: 'POST',
-            credentials: "include",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email
-            })
-        }).then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(data);
-        }).catch((err) => {
-            console.log(err);
-        });
-    };
+  const sendResetLink = () => {
+    fetch("http://localhost:8000/api/user/forgotPassword", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email
+      })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        if (data.success == true) {
+          changeSuccess("Mail sent successfully!");
+        } else {
+          changeError("Mail ID is incorrect!");
+        }
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <div>
-            <Navbar/>
-            <section className={'forgot-password-form'} style={{minHeight: '100vh'}}>
-                <Row style={{width: '100%', margin: 0}}>
-                    <Col md={1}/>
-                    <Col md={8}>
-                        <div className={'forgot-password-title'}>
-                            <b>Forgot Password</b>
-                        </div>
-                    </Col>
-                </Row>
-                <Row style={{width: '100%', paddingBottom: 60, margin: 0}}>
-                    <Col md={1}/>
-                    <Col md={4} className={'input-field-col'}>
-                        <input className={'input-field'} placeholder={'Email*'} value={email}
-                               onChange={(e) => changeEmail(e.target.value)} type="text"/>
-                    </Col>
-                </Row>
-                <Row style={{width: '100%', paddingBottom: 30, margin: 0}}>
-                    <Col md={1}/>
-                    <Col md={4}>
-                        <div className={'forgot-password-button'}>
-                            <a onClick={sendResetLink} className="sbtn sbtn-1 reset-link-btn">Send Reset link</a>
-                        </div>
-                    </Col>
-                </Row>
-            </section>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <section
+        className={"forgot-password-form"}
+        style={{ minHeight: "100vh" }}
+      >
+        <Row style={{ width: "100%", margin: 0 }}>
+          <Col md={1} />
+          <Col md={8}>
+            <div className={"forgot-password-title"}>
+              <b>Forgot Password</b>
+            </div>
+          </Col>
+        </Row>
+        <Row style={{ width: "100%", paddingBottom: 60, margin: 0 }}>
+          <Col md={1} />
+          <Col md={4} className={"input-field-col"}>
+            <input
+              className={"input-field"}
+              placeholder={"Email*"}
+              value={email}
+              onChange={e => changeEmail(e.target.value)}
+              type="text"
+            />
+          </Col>
+        </Row>
+        <Row style={{ width: "100%", paddingBottom: 30, margin: 0 }}>
+          <Col md={1} />
+          <Col md={4}>
+            <div className={"forgot-password-button"}>
+              <a onClick={sendResetLink} className="sbtn sbtn-1 reset-link-btn">
+                Send Reset link
+              </a>
+            </div>
+          </Col>
+        </Row>
+        <Row style={{ width: "100%", paddingBottom: 30, margin: 0 }}>
+          <Col md={1} />
+          <Col md={4}>
+            <p style={{ color: "#5ecd72", fontSize: "18px" }}>
+              Woah, what is dis?{successMessage}
+            </p>
+          </Col>
+        </Row>
+        <Row style={{ width: "100%", paddingBottom: 30, margin: 0 }}>
+          <Col md={1} />
+          <Col md={4}>
+            <p style={{ color: "#ff0000", fontSize: "18px" }}>
+              Hello there fellow human!{errorMessage}
+            </p>
+          </Col>
+        </Row>
+      </section>
+      <Footer />
+    </div>
+  );
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,131 +1,160 @@
-import * as React from 'react';
+import * as React from "react";
 import Footer from "./Footer";
-import Navbar from './Navbar';
-import {Col, Row} from "react-bootstrap";
-import '../styles/Login.css';
-import {
-    Redirect,
-} from "react-router-dom";
-
+import Navbar from "./Navbar";
+import { Col, Row } from "react-bootstrap";
+import "../styles/Login.css";
+import { Redirect } from "react-router-dom";
 
 export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            errorMessage: ""
-        };
-    }
-
-    login = () => {
-        const {username, password} = this.state;
-
-        fetch('http://localhost:8000/api/user/login', {
-            method: 'POST',
-            credentials: "include",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        }).then((response) => {
-            return response.json();
-        }).then((data) => {
-            console.log(data);
-            if (!data.success) {
-                this.changeErrorMessage(data.error);
-            } else {
-                this.props.login();
-            }
-        }).catch((error) => {
-            console.log(error);
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      errorMessage: ""
     };
+  }
 
-    changeUsername = (e) => {
-        this.setState({
-            username: e.target.value,
-            errorMessage: ""
-        });
-    };
+  login = () => {
+    const { username, password } = this.state;
 
-    changePassword = (e) => {
-        this.setState({
-            password: e.target.value,
-            errorMessage: ""
-        });
-    };
-
-    changeErrorMessage = (msg) => {
-        this.setState({
-            errorMessage: msg
-        });
-    };
-
-    render() {
-        const {isLoggedIn} = this.props;
-        const {username, password, errorMessage} = this.state;
-
-        console.log(isLoggedIn);
-
-        if (isLoggedIn) {
-            return <Redirect to='/'/>;
+    fetch("http://localhost:8000/api/user/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        if (!data.success) {
+          this.changeErrorMessage(data.error);
+        } else {
+          this.props.login();
         }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-        return (
-            <div>
-                <Navbar/>
-                <section className={'login-form'} style={{minHeight: '100vh'}}>
-                    <Row style={{width: '100%', margin: 0}}>
-                        <Col md={1}/>
-                        <Col md={8}>
-                            <div className={'login-title'}><b>Login</b></div>
-                        </Col>
-                    </Row>
-                    <Row style={{width: '100%', paddingBottom: 30, margin: 0}}>
-                        <Col md={1}/>
-                        <Col md={4} className={'input-field-col'}>
-                            <input className={'input-field'} placeholder={'Username*'} value={username}
-                                   onChange={this.changeUsername} type="text"/>
-                        </Col>
-                    </Row>
-                    <Row style={{width: '100%', paddingBottom: 30, margin: 0}}>
-                        <Col md={1}/>
-                        <Col md={4} className={'input-field-col'}>
-                            <input className={'input-field'} placeholder={'Password*'} value={password}
-                                   onChange={this.changePassword} type="password"/>
-                        </Col>
-                    </Row>
-                    <Row style={{width: '100%', margin: 0}}>
-                        <Col md={1}/>
-                        <Col md={6} className={'links'}>
-                            <p style={{color: 'red'}}>{errorMessage !== "" ? `*${errorMessage}` : null}</p>
-                        </Col>
-                    </Row>
-                    <Row style={{width: '100%', paddingBottom: 20, margin: 0}}>
-                        <Col md={1}/>
-                        <Col md={4} className={'input-field-col'}>
-                            <button onClick={this.login} className="sbtn sbtn-3 sbtn-3e icon-arrow-right login-btn">Enter</button>
-                        </Col>
-                    </Row>
-                    <Row style={{width: '100%'}}>
-                        <Col md={1}/>
-                        <Col md={6} className={'links'}>
-                            <p>New here? <a href={'/register'}><u>Register</u></a></p>
-                        </Col>
-                    </Row>
-                    <Row style={{width: '100%'}}>
-                        <Col md={1}/>
-                        <Col md={6} className={'links'}>
-                            <p>Forgot Password? <a><u>Reset here</u></a></p>
-                        </Col>
-                    </Row>
-                </section>
-                <Footer/>
-            </div>
-        );
+  changeUsername = e => {
+    this.setState({
+      username: e.target.value,
+      errorMessage: ""
+    });
+  };
+
+  changePassword = e => {
+    this.setState({
+      password: e.target.value,
+      errorMessage: ""
+    });
+  };
+
+  changeErrorMessage = msg => {
+    this.setState({
+      errorMessage: msg
+    });
+  };
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const { username, password, errorMessage } = this.state;
+
+    console.log(isLoggedIn);
+
+    if (isLoggedIn) {
+      return <Redirect to="/" />;
     }
+
+    return (
+      <div>
+        <Navbar />
+        <section className={"login-form"} style={{ minHeight: "100vh" }}>
+          <Row style={{ width: "100%", margin: 0 }}>
+            <Col md={1} />
+            <Col md={8}>
+              <div className={"login-title"}>
+                <b>Login</b>
+              </div>
+            </Col>
+          </Row>
+          <Row style={{ width: "100%", paddingBottom: 30, margin: 0 }}>
+            <Col md={1} />
+            <Col md={4} className={"input-field-col"}>
+              <input
+                className={"input-field"}
+                placeholder={"Username*"}
+                value={username}
+                onChange={this.changeUsername}
+                type="text"
+              />
+            </Col>
+          </Row>
+          <Row style={{ width: "100%", paddingBottom: 30, margin: 0 }}>
+            <Col md={1} />
+            <Col md={4} className={"input-field-col"}>
+              <input
+                className={"input-field"}
+                placeholder={"Password*"}
+                value={password}
+                onChange={this.changePassword}
+                type="password"
+              />
+            </Col>
+          </Row>
+          <Row style={{ width: "100%", margin: 0 }}>
+            <Col md={1} />
+            <Col md={6} className={"links"}>
+              <p style={{ color: "red" }}>
+                {errorMessage !== "" ? `*${errorMessage}` : null}
+              </p>
+            </Col>
+          </Row>
+          <Row style={{ width: "100%", paddingBottom: 20, margin: 0 }}>
+            <Col md={1} />
+            <Col md={4} className={"input-field-col"}>
+              <button
+                onClick={this.login}
+                className="sbtn sbtn-3 sbtn-3e icon-arrow-right login-btn"
+              >
+                Enter
+              </button>
+            </Col>
+          </Row>
+          <Row style={{ width: "100%" }}>
+            <Col md={1} />
+            <Col md={6} className={"links"}>
+              <p>
+                New here?{" "}
+                <a href={"/register"}>
+                  <u>Register</u>
+                </a>
+              </p>
+            </Col>
+          </Row>
+          <Row style={{ width: "100%" }}>
+            <Col md={1} />
+            <Col md={6} className={"links"}>
+              <p>
+                Forgot Password?{" "}
+                <a href={"/forgotpassword"}>
+                  <u>Reset here</u>
+                </a>
+              </p>
+            </Col>
+          </Row>
+        </section>
+        <Footer />
+      </div>
+    );
+  }
 }
