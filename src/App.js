aea491dba6612942ec7463a3fa3a4c4404eff7ca
AnjaneyaTripathi <anjaneya532000@gmail.com>
2020-01-23 00:51:50 +0530

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Dashboard from './Components/Dashboard';
-import './styles/App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./Components/Dashboard";
+import "./styles/App.css";
 import Login from "./Components/Login";
 import Register from "./Components/Register";
 import EventList from "./Components/EventList";
@@ -13,41 +13,42 @@ import ForgotPassword from "./Components/ForgotPassword";
 import ResetPassword from "./Components/ResetPassword";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoggedIn: false
-        };
-    }
-
-    componentDidMount() {
-        this.getIsLoggedIn();
-    }
-
-    changeIsLoggedIn = (isLoggedIn) => {
-        this.setState({
-            isLoggedIn
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false
     };
+  }
 
-    getIsLoggedIn = async () => {
-        const response = await fetch("http://localhost:8000/api/user/isLoggedIn", {
-            credentials: "include",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        });
-        const data = await response.json();
-        this.setState({
-            isLoggedIn: data.isLoggedIn
-        });
-        console.log(data);
-    };
+  componentDidMount() {
+    this.getIsLoggedIn();
+  }
 
-    render() {
-        const {isLoggedIn} = this.state;
+  changeIsLoggedIn = isLoggedIn => {
+    this.setState({
+      isLoggedIn
+    });
+  };
 
+  getIsLoggedIn = async () => {
+    const response = await fetch("http://localhost:8000/api/user/isLoggedIn", {
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await response.json();
+    this.setState({
+      isLoggedIn: data.isLoggedIn
+    });
+    console.log(data);
+  };
+
+  render() {
+    const { isLoggedIn } = this.state;
+
+<<<<<<< HEAD
         return (
             <div className="App">
                 <Router>
@@ -71,6 +72,74 @@ class App extends Component {
             </div>
         );
     }
+=======
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Route
+              exact
+              path="/"
+              render={() => <Dashboard isLoggedIn={isLoggedIn} />}
+            />
+            <Route
+              exact
+              path="/login"
+              render={() => (
+                <Login
+                  isLoggedIn={isLoggedIn}
+                  login={() => {
+                    this.changeIsLoggedIn(true);
+                  }}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/register"
+              render={() => <Register isLoggedIn={isLoggedIn} />}
+            />
+            <Route
+              exact
+              path="/events"
+              render={() => <EventList isLoggedIn={isLoggedIn} />}
+            />
+            <Route
+              exact
+              path="/details"
+              render={() => <Details isLoggedIn={isLoggedIn} />}
+            />
+            <Route
+              exact
+              path="/workshops"
+              render={() => <WorkshopList isLoggedIn={isLoggedIn} />}
+            />
+            <Route
+              exact
+              path="/profile"
+              render={() => (
+                <Profile
+                  logout={() => {
+                    this.changeIsLoggedIn(false);
+                  }}
+                />
+              )}
+            />
+            <Route
+              path="/verifyuser/:code"
+              render={({ match }) => <VerifyEmail code={match.params.code} />}
+            />
+            <Route path="/forgotpassword" render={() => <ForgotPassword />} />
+            <Route
+              path="/resetpassword/:code"
+              render={({ match }) => <ResetPassword code={match.params.code} />}
+            />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+>>>>>>> added google analytics
 }
 
 export default App;
